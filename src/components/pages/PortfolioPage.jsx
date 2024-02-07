49190f2c08cbab6adce9e7872a75f95bce2a24d8
Brian King <Thebeakeh@gmail.com>
2024-02-07 16:27:03 -0600

    vite2

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -1,12 +1,4 @@
-// import { useState, useEffect } from 'react';
-// // Bringing in the required component from 'react-router-dom' for linking between pages and getting the current param variable's value from URL
-// import { useParams, Link } from 'react-router-dom';
-
-
 import PortfolioCard from '../PortfolioCard';
-
-
-
 const portfolioProjectsArray = [
   {
     key: 1,
@@ -18,7 +10,7 @@ const portfolioProjectsArray = [
 
  {
   key: 2,
-  img:"public/blog-site.png",
+  img:"dist/assets/blog-site.png",
   title:'Blog-Site',
   repoLink:'https://github.com/kingbgreen5/blog-site',
   deployed: 'https://blog-site-bk-77edcaeea42f.herokuapp.com/'
