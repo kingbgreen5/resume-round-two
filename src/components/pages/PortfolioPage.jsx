f1cb1dd89756101c30c0d94692e619499f4faf37
Brian King <Thebeakeh@gmail.com>
2024-02-07 22:11:40 -0600

    finally hopefully

--- src/components/pages/PortfolioPage.jsx
+++ src/components/pages/PortfolioPage.jsx
@@ -2,6 +2,10 @@ import PortfolioCard from '../PortfolioCard';
 
 import marsPicture from "/assets/mars.png";
 import blogPicture from "/assets/blog-site.png";
+import monsterMine from "/assets/monster-mine.png";
+import workDay from "/assets/work-day.png";
+import noteTaker from "/assets/note-taker.png"
+import codeQuiz from "/assets/code-quiz.png"
 
 const portfolioProjectsArray = [
   {
@@ -21,28 +25,28 @@ const portfolioProjectsArray = [
 },
  {
   key: 3,
-  img:"/monster-mine.png",
+  img:monsterMine,
   title:'Monster Mine: Collaborative Project',
    repoLink:'https://github.com/kingbgreen5/Monster-Mineshaft',
   deployed: 'https://cryptic-bastion-38379-0ef3d47a3413.herokuapp.com/'
 },
  {
   key: 4,
-  img:"/work-day.png",
+  img:workDay,
   title:'Work-Day-Scheduler',
   repoLink:'https://github.com/kingbgreen5/work-day-schedule',
    deployed: 'https://kingbgreen5.github.io/work-day-schedule/'
 },
  {
   key: 5,
-  img:"public/note-taker.png",
+  img:noteTaker,
   title:'Note-Taker',
   repoLink:'https://github.com/kingbgreen5/note-taker',
   deployed: 'https://floating-hamlet-15376-a415bce60b61.herokuapp.com/notes'
 },
 {
   key: 6,
-  img:"public/code-quiz.png",
+  img:codeQuiz,
   title:'Code-Quiz',
   repoLink:'https://github.com/kingbgreen5/code-quiz',
   deployed: 'https://kingbgreen5.github.io/code-quiz/'
