d6f6cad50ec9d0d73c1d695177c2833f4827bac7
Brian King <Thebeakeh@gmail.com>
2024-02-07 21:58:15 -0600

    gonna work this time

--- src/components/pages/AboutMePage.jsx
+++ src/components/pages/AboutMePage.jsx
@@ -1,6 +1,6 @@
-// This is a static page mocking an "About Us" section for our fake user data
 
-// import photo from "../../../src/assets/photo"
+
+import photo from "../../../public/assets/profilephoto.jpg"
 
 export default function AboutPage() {
 return(
@@ -8,13 +8,13 @@ return(
   <div>
     <br />
 
- <img className="aboutme-photo"  src= "/assets/profilephoto.jpg" alt=""/>
+ <img className="aboutme-photo"  src = {photo} alt=""/>
  <br />
  <br />
  <br />
  <div class="aboutme">
   Hello, Im Brian.
-  Im a Full Stack Web Development Bootcamp student. Currentlying learning MERN stack and close to graduating form the program. I enjoy learning new aspects of
+  Im a Full Stack Web Development Bootcamp student. Currently learning MERN stack and close to graduating form the program. I enjoy learning new aspects of
   Web Development and working on coding projects. After seeing a demonstration by a friend, I've been working on learning VIM motions little by little.
   In my spare time I enjoy working on Video Game Development, Woodworking, and Traditional Archery. My wife and I have a 10 month old baby girl named Ivy and
   our second child due in June!
