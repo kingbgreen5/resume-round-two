4e6a6f4f0236f0d28cc97ee85012c6d7f3e8dfed
Brian King <Thebeakeh@gmail.com>
2024-02-07 21:44:26 -0600

    testing2

--- src/components/pages/AboutMePage.jsx
+++ src/components/pages/AboutMePage.jsx
@@ -8,7 +8,7 @@ return(
   <div>
     <br />
 
- <img className="aboutme-photo"  src= "/assets/photo.jpg" alt=""/>
+ <img className="aboutme-photo"  src= "/assets/profilephoto.jpg" alt=""/>
  <br />
  <br />
  <br />
